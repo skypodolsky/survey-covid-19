58371dbfe563e7e10dec5daf2685f5168eaf1df5
Stanislav Barantsev <stanislav.barantsev@community.isunet.edu>
2021-12-24 09:15:28 +0100

    Updated scripts

--- build_classification.py
+++ build_classification.py
@@ -66,58 +66,66 @@ antivax_stats_df = antivax_df = df[df['Ви вакциновані:'] == 'Ні']
 
 df.to_csv('survey-final.csv', sep='\t', index=False)
 
-for df, name in [(vax_stats_df, 'vax')]: #, (antivax_stats_df, 'antivax')]:
+for df, name in [(vax_stats_df, 'vax'), (antivax_stats_df, 'antivax')]:
+    for classification, desc in [ ('basic_classification', 'Розподіл вибірки респондентів за стандартною класифікацією професій'), ('modified_classification', 'Розподіл вибірки респондентів за модифікованою класифікацією професій') ]:
 
-    counts_series = df['basic_classification'].value_counts()
-    labels = counts_series.index.tolist()
-    counts = counts_series.values.tolist()
-    percents = counts / counts_series.sum() * 100
-    percents = percents.tolist()
+        counts_series = df[classification].value_counts()
+        labels = counts_series.index.tolist()
+        counts = counts_series.values.tolist()
+        percents = counts / counts_series.sum() * 100
+        percents = percents.tolist()
 
-    print(percents)
-    print('===')
-    other = 0
-    changed = False
-    for percent in reversed(percents):
-        if percent <= 5:
-            changed = True
-            other += percent
-            print('drop percentage {}'.format(percent))
-            #  counts.pop()
-            percents.pop()
-            labels.pop()
+        print(percents)
+        print(labels)
+        print('===')
+        other = 0
+        changed = False
+        for percent in reversed(percents):
+            if percent <= 5:
+                changed = True
+                other += percent
+                print('drop percentage {}'.format(percent))
+                #  counts.pop()
+                percents.pop()
+                labels.pop()
 
-    if changed:
-        percents.append(other)
-        labels.append('Інші')
+        if changed:
+            percents.append(other)
+            labels.append('Інші')
 
-    print(percents)
-    res = zip(labels, percents)
+        print(percents)
+        res = zip(labels, percents)
 
-    fin_labels = []
-    for i, j in res:
-        fin_labels.append('{:} - {:.2f} %'.format(i, j))
+        fin_labels = []
+        for i, j in res:
+            fin_labels.append('{:} - {:.2f} %'.format(i, j))
 
-    fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
+        fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
 
-    data = percents
+        wedges, texts = ax.pie(percents, wedgeprops=dict(width=0.5), startangle=-40)
 
-    wedges, texts = ax.pie(data, wedgeprops=dict(width=0.5), startangle=-40)
+        bbox_props = dict(boxstyle="square,pad=0.1", fc="w", ec="k", lw=0.72)
+        kw = dict(arrowprops=dict(arrowstyle="-"),
+              bbox=bbox_props, zorder=0, va="center")
 
-    bbox_props = dict(boxstyle="square,pad=0.1", fc="w", ec="k", lw=0.72)
-    kw = dict(arrowprops=dict(arrowstyle="-"),
-          bbox=bbox_props, zorder=0, va="center")
+        for i, p in enumerate(wedges):
+            ang = (p.theta2 - p.theta1)/2. + p.theta1
+            y = np.sin(np.deg2rad(ang))
+            x = np.cos(np.deg2rad(ang))
+            horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+            connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+            kw["arrowprops"].update({"connectionstyle": connectionstyle})
+            ax.annotate(fin_labels[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
+                    horizontalalignment=horizontalalignment, **kw)
 
-    for i, p in enumerate(wedges):
-        ang = (p.theta2 - p.theta1)/2. + p.theta1
-        y = np.sin(np.deg2rad(ang))
-        x = np.cos(np.deg2rad(ang))
-        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
-        connectionstyle = "angle,angleA=0,angleB={}".format(ang)
-        kw["arrowprops"].update({"connectionstyle": connectionstyle})
-        ax.annotate(fin_labels[i], xy=(x, y), xytext=(1.35*np.sign(x), 1.4*y),
-                horizontalalignment=horizontalalignment, **kw)
+        #  ax.set_title(desc)
 
-    ax.set_title("Розподіл вибірки респондентів за стандартною класифікацією професій")
+        mng = plt.get_current_fig_manager()
+        mng.full_screen_toggle()
 
-    plt.show()
+        plt.show()
+
+        if classification == "basic_classification":
+            fig.savefig('classification-' + name + '-standard', dpi=200)
+        else:
+            fig.savefig('classification-' + name + '-modified', dpi=200)
